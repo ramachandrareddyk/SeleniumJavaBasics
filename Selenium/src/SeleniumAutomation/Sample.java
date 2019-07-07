4fe2ed8ea544dfa006f3fe6d91a01e5a327b201b
USER-PC <USER-PC@192.168.0.4>
2019-07-07 17:23:34 +0530

    New page added

---
+++ Selenium/src/SeleniumAutomation/Sample.java
@@ -0,0 +1,15 @@
+package SeleniumAutomation;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	
+		@Test
+		public void M1(){
+			System.out.println("'Ramachandra is an automation engineer'");
+		}
+		
+
+	
+}
