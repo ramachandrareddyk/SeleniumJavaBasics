40d83a139984f49ae5618b3573a07d188d3921f9
Ramachandra Reddy <ramureddy4555@gmail.com>
2019-07-15 23:52:08 +0530

    Changes

--- Selenium/src/Dataprovider/HomePage.java
+++ Selenium/src/Dataprovider/HomePage.java
@@ -2,8 +2,10 @@ package Dataprovider;
 
 public class HomePage {
 
+	public void main(String)
 	
 	void m1(){
 		System.out.println("Ramachandra and sathish");
+		System.out.println("Ramachandra and sathish good friends");
 	}
 }
