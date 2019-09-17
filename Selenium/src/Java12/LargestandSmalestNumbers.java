e667db027a2477f8d7a6a19c4ec79ccb7ba2012f
Ramachandra Reddy <ramureddy4555@gmail.com>
2019-09-17 20:46:58 +0530

    New changes

--- Selenium/src/Java12/LargestandSmalestNumbers.java
+++ Selenium/src/Java12/LargestandSmalestNumbers.java
@@ -5,8 +5,8 @@ public class LargestandSmalestNumbers {
 	public static void main(String[] args) {
 		
 		int a[] = { 23, 34, 13, 64, 72, 90, 10, 15, 9, 27 };
-		int min=0;
-		int max=0;
+		int min=a[0];
+		int max=a[0];
 		for (int i = 0; i < a.length; i++) {
 			if(a[i]>max){
 				max=a[i];
